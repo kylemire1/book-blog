11a2fb322c29dcbad4be98b0efaf655029908bfb
Kyle Lemire <kyle@elykinnovation.com>
2019-09-08 20:18:45 -0500

    Preparing for deployment

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const port = process.env.PORT || 5000;
 
 app.use("/api/v1/", posts);
 
+if (process.env.NODE_ENV === "production") {
+  // ... other imports
+  const path = require("path");
+
+  // ... other app.use middleware
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  // ...
+  // Right before your app.listen(), add this:
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, (req, res) => {
   console.log(`Server listening on port ${port}`);
 });
