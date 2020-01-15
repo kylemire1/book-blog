c55323e4476c99c781ad83bd2f005d875ac339f9
Kyle Lemire <kyle@elykinnovation.com>
2020-01-14 20:07:13 -0600

    feat: setting up Contentful integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
+  ]
+};
