6adc9c040493ba02c5ac30adec2f714bae3d92fb
Kyle Lemire <kyle@elykinnovation.com>
2020-01-18 18:17:46 -0600

    chore: temp disabling PWA features

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,18 +28,18 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `BookWorm`,
-        short_name: `BookWorm`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#B273FE`,
-        display: `standalone`
-      }
-    },
-    `gatsby-plugin-offline`
+    }
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `BookWorm`,
+    //     short_name: `BookWorm`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#B273FE`,
+    //     display: `standalone`
+    //   }
+    // },
+    // `gatsby-plugin-offline`
   ]
 };
